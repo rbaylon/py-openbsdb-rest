ca9ce9d889e376eac77193b790c57acbed20f795
eking <eking@live.com.ph>
2022-03-11 04:37:59 +0800

    remove frontend modules

--- views/vmviews.py
+++
@@ -1,9 +0,0 @@
-from views import app
-from views.apiviews import token_required
-from flask import request, redirect, url_for, jsonify, make_response
-
-
-@app.route('/api/vm', methods=['GET'])
-@token_required
-def vms(current_user):
-    return jsonify({'Virtual Machines' : ['vm1', 'vm2']})
